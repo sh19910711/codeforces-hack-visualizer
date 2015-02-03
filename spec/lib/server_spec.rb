15701e737623f57f944615d5ce4980c043cdf1b6
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-03 18:42:27 +0900

    test: add tests for contest not found case

--- spec/lib/server_spec.rb
+++ spec/lib/server_spec.rb
@@ -19,7 +19,15 @@ describe ::Server do
     context "GET /contests/-/123" do
       before { get "/contests/-/123" }
       subject { last_response }
-      it { should be_ok }
+      it { should_not be_ok }
+      context "create contest" do
+        before { ::Models::Contest.create :id => 123 }
+        context "GET /contests-/123" do
+          before { get "/contests/-/123" }
+          subject { last_response }
+          it { should be_ok }
+        end
+      end
     end
 
   end
